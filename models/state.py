6e7dcf7f3c2e348e43d7432f0e36a5f8d7496d11
altoid <noelreeds01@gmail.com>
2025-01-31 14:34:23 +0000

    adds a ref for file_storage

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import Column, String, relationship
 
@@ -8,4 +9,15 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    cities = relationship('City', back_populates='states', passive_deletes=True)
+    if os.getenv('HBNB_TYPE_STORAGE') is "db":
+        cities = relationship('City', back_populates='states',
+                                passive_deletes=True)
+    else:
+        from models import storage
+        cities = []
+        # retrieve the dictionary of objects
+        objs = storage.all()
+        for key, item in objs.items():
+            if item.id is item.state_id:
+                cities.append(item.name)
+        return cities
