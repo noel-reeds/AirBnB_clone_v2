3d2434722f66f259068c287ab08dfa353b32bd4a
altoid <noelreeds01@gmail.com>
2025-02-03 08:22:21 +0000

    adds correct refs

--- models/state.py
+++ models/state.py
@@ -2,22 +2,24 @@
 """ State Module for HBNB project """
 import os
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import Column, String, relationship
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    if os.getenv('HBNB_TYPE_STORAGE') is "db":
-        cities = relationship('City', back_populates='states',
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship('City', back_populates='state',
                                 passive_deletes=True)
     else:
-        from models import storage
-        cities = []
-        # retrieve the dictionary of objects
-        objs = storage.all()
-        for key, item in objs.items():
-            if item.id is item.state_id:
-                cities.append(item.name)
-        return cities
+        def get_cities(self):
+            from models import storage
+            cities = []
+            # retrieve the dictionary of objects
+            objs = storage.all()
+            for key, item in objs.items():
+                if item.id is item.state_id:
+                    cities.append(item.name)
+            return cities
