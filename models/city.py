71f017721c91efb0dedb51003b7f704fe9a2a4be
altoid <noelreeds01@gmail.com>
2025-02-03 13:00:20 +0000

    adds a reference between the classes

--- models/city.py
+++ models/city.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import ForeignKey, relationship
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-
-    state_id = Column(String(60), nullable=False,
-                    ForeignKey('states.id', ondelete='CASCADE'))
-    name = Column(String(128), nullable=False)
-    state = relationship('State', back_populates='cities')
+    # configure database for db_storage
+    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        state_id = Column(String(60),
+                   ForeignKey('states.id', ondelete='CASCADE'), nullable=False)
+        name = Column(String(128), nullable=False)
+        state = relationship('State', back_populates='cities')
+    else:
+        from models.state import State
+        state_id = State.id
+        name = ""
--- models/state.py
+++ models/state.py
@@ -8,18 +8,21 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-
-    name = Column(String(128), nullable=False)
+    # for db_storage db, configure database.
     if os.getenv('HBNB_TYPE_STORAGE') == "db":
+        name = Column(String(128), nullable=False)
         cities = relationship('City', back_populates='state',
                                 passive_deletes=True)
     else:
-        def get_cities(self):
-            from models import storage
-            cities = []
-            # retrieve the dictionary of objects
-            objs = storage.all()
-            for key, item in objs.items():
-                if item.id is item.state_id:
-                    cities.append(item.name)
-            return cities
+        name = ""
+        cities = []
+    
+    @property
+    def cities(self):
+        from models import storage
+        cities = []
+        # retrieve the dictionary of objects
+        for key, item in storage.all(City).items():
+            if item.id is item.state_id:
+                cities.append(item.name)
+        return cities
