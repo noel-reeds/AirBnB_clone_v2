a99feb75c804e34745b8fa769b1564bc4e32ee4a
altoid <noelreeds01@gmail.com>
2025-02-17 14:19:46 +0000

    configures nginx to serve static content

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+# installs nginx ubuntu server
+if [ ! -x /usr/sbin/nginx ]; then
+    echo "nginx not installed.."
+    echo "installing nginx.."
+    sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+    curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+        | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+    echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+    http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
+        | sudo tee /etc/apt/sources.list.d/nginx.list
+
+    sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt install nginx
+fi
+
+# start nginx
+sudo service nginx start
+
+# create /data/ folders
+if [ ! -d "/data/" ]; then
+    sudo mkdir /data/
+    sudo mkdir /data/web_static/
+    sudo mkdir /data/web_static/releases/
+    sudo mkdir /data/web_static/shared/
+    sudo mkdir /data/web_static/releases/test/
+    sudo touch /data/web_static/releases/test/index.html
+    # append fake HTML content to test Nginx configurations
+    sudo echo
+        "<html>
+        <head>
+        </head>
+            <body>
+                ALX
+            </body>
+        </html>" > /data/web_static/releases/test/index.html
+fi
+
+# creates a symlink
+if [ -L "/data/web_static/current" ]; then
+    # delete already existing symlink
+    sudo rm /data/web_static/current
+    sudo ln -s "/data/web_static/releases/test/" "/data/web_static/current"
+else
+    sudo ln -s "/data/web_static/releases/test/" "/data/web_static/current"
+fi
+
+# give ownership of the /data/ folder
+sudo chown -R ubuntu:ubuntu /data/
+
+# location directive to serve static content
+dir="\n\tserver {\n\t\tlocation /hbnb_static {\n\t\t\talias /data/web_static/current\n\t\t}\n\t}\n}"
+
+# configure Nginx to serve /data/web_static/current/ to hbnb_static
+sudo sed -i "32i\\$dir" /etc/nginx/nginx.conf
+
+# restart nginx
+sudo service nginx -s reload
