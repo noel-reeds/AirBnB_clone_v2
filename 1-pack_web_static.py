03826030cba31bf33b8a3aa7d3593a93e4e910b9
altoid <noelreeds01@gmail.com>
2025-02-20 10:02:59 +0000

    sorts a req failure

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# creates an archive from web_static
+"""creates an archive from web_static"""
 import os, tarfile
 from datetime import datetime as dt
 from fabric.api import local, run
 
 def do_pack():
-    # check if versions dir exists and create.
+    """check if versions dir exists and create."""
     if not os.path.isdir('./versions'):
         local("mkdir versions")
     _filepath = f"versions/web_static_{dt.now().strftime('%Y%m%d%H%M%S')}.tgz"
