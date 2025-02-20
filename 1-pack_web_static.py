0a34b57cf0cf74e85664a7d30039e77a8eb7f844
altoid <noelreeds01@gmail.com>
2025-02-20 10:01:13 +0000

    creates an archive of web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# creates an archive from web_static
+import os, tarfile
+from datetime import datetime as dt
+from fabric.api import local, run
+
+def do_pack():
+    # check if versions dir exists and create.
+    if not os.path.isdir('./versions'):
+        local("mkdir versions")
+    _filepath = f"versions/web_static_{dt.now().strftime('%Y%m%d%H%M%S')}.tgz"
+    local(f"tar -cvzf {_filepath} web_static")
+    if not tarfile.is_tarfile(_filepath):
+        return None
+    else:
+        print(f"web_static packed: {_filepath} -> {os.path.getsize(_filepath)}Bytes")
