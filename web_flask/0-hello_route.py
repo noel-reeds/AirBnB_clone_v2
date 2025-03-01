bcb6cc6d0aa0fb3ff81f43a31a3c8df835ea610d
altoids <noelreeds01@gmail.com>
2025-03-01 05:44:47 -0800

    PEP8 validated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,13 +2,14 @@
 """a minimal app in Flask"""
 from flask import Flask
 
-
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello():
     """a basic string returned in Flask"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
