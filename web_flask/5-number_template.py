758a098bc86f56fd7c75a3b86d51a8d2d3dbf509
altoids <noelreeds01@gmail.com>
2025-03-01 12:49:26 -0800

    renders a page for a specified converter

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""a minimal app in Flask"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello():
+    """A basic string returned in Flask"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Returns hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def var_hbnb(text):
+    """display “C ” followed by the value of the text variable"""
+    if "_" in text:
+        text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python", defaults={'text': None})
+@app.route("/python/<text>")
+def def_hbnb(text):
+    """display “Python ”, followed by the value of the text variable"""
+    if not text:
+        return "Python is cool"
+    elif "_" in text:
+        text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>")
+def is_it_number(n):
+    """display “n is a number” only if n is an integer"""
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>")
+def number_template(n):
+    """display a HTML page only if n is an integer"""
+    return render_template("5-number.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
