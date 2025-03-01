2e8013ce50266b103a5140d3290753fec7421da1
altoids <noelreeds01@gmail.com>
2025-03-01 11:47:59 -0800

    specifies default routes

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""a minimal app in Flask"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello():
+    """A basic string returned in Flask"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Returns hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def var_hbnb(text):
+    """display “C ” followed by the value of the text variable"""
+    if "_" in text:
+        text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python", defaults={'text': None})
+@app.route("/python/<text>")
+def def_hbnb(text):
+    """display “Python ”, followed by the value of the text variable"""
+    if not text:
+        return "Python is cool"
+    elif "_" in text:
+        text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
