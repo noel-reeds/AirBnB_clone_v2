56389d6f0a39f989f7e455ad083469612b99dfc9
altoids <noelreeds01@gmail.com>
2025-03-06 13:20:35 +0300

    creates a place_amenity table

--- models/place.py
+++ models/place.py
@@ -8,11 +8,15 @@ from sqlalchemy.orm import relationship
 
 # an association table for many-to-many rlship
 place_amenity = Table('place_amenity', Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'),
-                        primary_key=True, nullable=False),
-    Column('amenity_id',
-    String(60), ForeignKey('amenities.id'),
-                        primary_key=True, nullable=False))
+                    Column('place_id', String(60),
+                        ForeignKey('places.id'),
+                        primary_key=True,
+                        nullable=False),
+                    Column('amenity_id',
+                        String(60),
+                        ForeignKey('amenities.id'),
+                        primary_key=True,
+                        nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
